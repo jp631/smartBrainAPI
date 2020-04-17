e6f543a74b356393841f2dc93b6b38686f4312c2
jp631 <47936264+jp631@users.noreply.github.com>
2020-04-17 16:28:18 -0400

    printing error to screen to find error

--- register.js
+++ register.js
@@ -26,7 +26,7 @@ const handleRegister = (req, res, db, bcrypt) => {
     .then(trx.commit)
     .catch(trx.rollback)
   })
-  .catch(err => res.status(400).json("Unable To Register!"));
+  .catch(err => res.status(400).json(`Unable To Register! ${err}`));
 }
 
 module.exports = {
