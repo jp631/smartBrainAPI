8c8129f22b3228dd7bed0f8418222570cc703385
jp631 <47936264+jp631@users.noreply.github.com>
2020-04-17 17:10:34 -0400

    fixing ssl issue

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const knex = require("knex");
 const register = require("./register")
 const  clarifai = require('clarifai');
+process.env.MODE_ITS_REJECT_UNAUTHORIZED = 0;
 
 let PORT = process.env.PORT;
 const app = express();
@@ -17,7 +18,7 @@ const app = express();
    const db = knex({
    client: 'pg',
    connection: {
-     connectionString : process.env.DATABSE_URL,
+    connectionString : process.env.DATABSE_URL,
      ssl: true
    }
  });
